3874949928e48c89562a3f5b294c58170ef54b75
Yibo Guo <nil.yibo@gmail.com>
2023-04-26 16:20:47 -0700

    Added script to query carbon scheduler API.

---
+++ cli/carbon-api/submit.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ $# -lt 1 ]; then
+    echo >&2 "Usage: $0 path/to/body.json"
+    exit 1
+fi
+
+json_file="$1"
+
+if ! [ -f "$json_file" ]; then
+    echo >&2 "JSON file \"$json_file\" not found."
+    exit 1
+fi
+
+CARBON_API_URL=https://cas-carbon-api.nrp-nautilus.io/carbon-aware-scheduler/
+
+set -x
+
+curl -s -X GET -H "Content-Type: application/json" -d @"$json_file" $CARBON_API_URL | jq
