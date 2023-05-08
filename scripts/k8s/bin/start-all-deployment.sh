81656e49bfb4db9c2d01544752c7251a287b871e
Yibo Guo <nil.yibo@gmail.com>
2023-05-08 11:44:01 -0700

    Added script to start/stop kuberenetes deployment.

---
+++ scripts/k8s/bin/start-all-deployment.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd "$(dirname "$0")/../../../src/kubernetes" || { echo >&2 "Cannot cd to kubernetes directory."; exit 1; }
+
+find . -type f -name '*.deployment.yaml' -exec kubectl create -f {} \;
