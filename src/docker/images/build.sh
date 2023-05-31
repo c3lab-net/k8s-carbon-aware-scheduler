ceb63861e26f065ed17fdae384641c745f96a494
Yibo Guo <nil.yibo@gmail.com>
2023-05-31 16:41:12 -0700

    Append suffix of Dockerfile to image name.

--- src/docker/images/build.sh
+++ src/docker/images/build.sh
@@ -13,11 +13,12 @@ build_image()
     PROJECT_NAME="$1"
     IMAGE_NAME="$2"
     IMAGE_VERSION="${3}"
+    DOCKERFILE="${4}"
 
     FULL_IMAGE_NAME=gitlab-registry.nrp-nautilus.io/c3lab/$PROJECT_NAME/$IMAGE_NAME:$IMAGE_VERSION
 
     echo "Building image $FULL_IMAGE_NAME ..."
-    (set -x; docker build -t $FULL_IMAGE_NAME .)
+    (set -x; docker build -t $FULL_IMAGE_NAME -f "$DOCKERFILE" .)
 
     echo Done
     echo "Run this to push to registry:"
@@ -25,9 +26,12 @@ build_image()
 }
 
 cwd="$(pwd)"
-for f in **/Dockerfile; do
-    relpath=$(dirname "$f")
+for f in **/Dockerfile*; do
+    filename="$(basename "$f")"
+    suffix="${filename##Dockerfile}"
+    relpath="$(dirname "$f")"
     component="${relpath:gs/\//.}"
-    cd "$relpath" && build_image carbon-aware-scheduler "$component" "$VERSION"
+    [ -z "$suffix" ] || component+="$suffix"
+    cd "$relpath" && build_image carbon-aware-scheduler "$component" "$VERSION" "$filename"
     cd "$cwd"
 done
